2d2246407fc7586c2ff73f7f757bca95c502b596
tranvankhuyen <khuyendvdtht@gmail.com>
2023-05-11 04:09:14 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import About from "../pages/About";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
